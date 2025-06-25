9321288da7ed40295d1256c366a9f1bc19445f9a
Saransh Sharma <Saransh Sharma>
2025-06-25 19:50:11 +0300

    chore: Finalize UI fixes and .gitignore

--- src/partner/app/components/top-navbar.tsx
+++ src/partner/app/components/top-navbar.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import { Bell } from "@medusajs/icons"
+import { BellAlert } from "@medusajs/icons"
 import { IconButton } from "@medusajs/ui"
 
 export const TopNavbar = () => {
   return (
     <header className="bg-ui-bg-subtle flex h-14 items-center justify-end gap-x-4 border-b px-8">
       <IconButton>
-        <Bell />
+        <BellAlert />
       </IconButton>
     </header>
   )
