c061ebdac91a4f41ddf4813a1834db4537d11e52
Saransh Sharma <Saransh Sharma>
2025-08-04 18:16:16 +0530

    revert(Stripe Dev Settings): Reversed stripe dev settings

--- medusa-config.ts
+++ medusa-config.ts
@@ -122,20 +122,20 @@ module.exports = defineConfig({
         ],
       },
     },
-    {
-      resolve: "@medusajs/medusa/payment",
-      options: {
-        providers: [
-          {
-            resolve: "@medusajs/medusa/payment-stripe",
-            id: "stripe",
-            options: {
-              apiKey: process.env.STRIPE_API_KEY,
-            },
-          },
-        ],
-      },
-    },
+    // {
+    //   resolve: "@medusajs/medusa/payment",
+    //   options: {
+    //     providers: [
+    //       {
+    //         resolve: "@medusajs/medusa/payment-stripe",
+    //         id: "stripe",
+    //         options: {
+    //           apiKey: process.env.STRIPE_API_KEY,
+    //         },
+    //       },
+    //     ],
+    //   },
+    // },
   {
     resolve: "./src/modules/agreements",
   },
